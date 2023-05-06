ba835f4b85981b3f335ec7ab9be533a3df562d03
Abdul R <abdul.kasmo.iv@outlook.com>
2023-05-06 11:36:58 -0500

    add styling to Index.jsx file

--- views/pokemons/New.jsx
+++ views/pokemons/New.jsx
@@ -1,20 +1,20 @@
-const React = require("react");
-const Nav = require("../components/Nav");
+// const React = require("react");
+// const Nav = require("../components/Nav");
 
-class New extends React.Component {
-  render() {
-    return (
-      <div>
-        <Nav link="/pokemons" text="Home" />
-        <h1>Create a New Pokemon!</h1>
-        <form action="/pokemons" method="POST">
-          Name: <input type="text" name="name" />
-          image: <input type="text" name="img" />
-          <input type="submit" value="Create Pokemon" />
-        </form>
-      </div>
-    );
-  }
-}
+// class New extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <Nav link="/pokemons" text="Home" />
+//         <h1>Create a New Pokemon!</h1>
+//         <form action="/pokemons" method="POST">
+//           Name: <input type="text" name="name" />
+//           image: <input type="text" name="img" />
+//           <input type="submit" value="Create Pokemon" />
+//         </form>
+//       </div>
+//     );
+//   }
+// }
 
-module.exports = New;
+// module.exports = New;
